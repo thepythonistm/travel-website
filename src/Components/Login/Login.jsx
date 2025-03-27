9aabf09c4ab71510f4fd6eb469004c693f9141ca
thepythonistm <thepythonistm@gmail.com>
2025-03-27 16:09:12 -0700

    Updated login UI and added background images

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import './Login.css';
 const Login = () => {
     return(
-        <div></div>
+        <div className="login-container">
+            <h1>Login</h1>
+            <input placeholder="username" type="text" required className="login-username" /><br />
+            <input placeholder="password" type="password" required className="login-password" /><br />
+            <button className="login-btn">Login</button>
+
+        </div>
     )
 
 }
